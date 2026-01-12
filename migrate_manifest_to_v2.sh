474e50908a5ee4b62b1d2be7ac241c9384fc35c9
m0ranmcharles <m0ranmcharles@gmail.com>
2026-01-12 00:18:44 -0500

    removed shell implementation, rust rebuild is now the sole and primary build

--- migrate_manifest_to_v2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# migrate_manifest_to_v2.sh
-# Purpose: Upgrade legacy manifests to v2 TSV format.
-# Current: Placeholder as we are starting with v2.
-
-echo "No legacy manifests to migrate."
-exit 0
